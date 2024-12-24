ffc82bec98bc52f1e912667b77450064ad260a89
setanoier <setanoier@gmail.com>
2024-12-24 18:45:17 +0300

    refactor: migrate from math package

--- pkg/clamp.go
+++ pkg/clamp.go
@@ -1,8 +1,6 @@
 package pkg
 
-import "math"
-
 // ClampToUint8 clamps an integer to the range [0, 255].
 func ClampToUint8(value int) uint8 {
-	return uint8(math.Max(0, math.Min(float64(value), 255)))
+	return uint8(max(0, min(value, 255))) //nolint
 }
