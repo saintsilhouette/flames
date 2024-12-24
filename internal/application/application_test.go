6502cfe9a1c1255b9b92e01617b88b80480aeaa2
setanoier <setanoier@gmail.com>
2024-12-25 00:30:28 +0300

    test: add benchmark for the render method

--- internal/application/application_test.go
+++ internal/application/application_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func benchmarkRender(b *testing.B) {
-	cfg, _ := config.New(1920, 1080, 2048, 32, uint(b.N), "images") //nolint
+	cfg, _ := config.New(1920, 1080, 2048, 32, uint(b.N), 0, config.Directory) //nolint
 	app := application.New(cfg)
 
 	app.Linear = []*linear.Linear{
@@ -37,5 +37,29 @@ func BenchmarkRender16(b *testing.B) {
 }
 
 func BenchmarkRender32(b *testing.B) {
+	benchmarkRender(b)
+}
+
+func BenchmarkRender64(b *testing.B) {
+	benchmarkRender(b)
+}
+
+func BenchmarkRender128(b *testing.B) {
+	benchmarkRender(b)
+}
+
+func BenchmarkRender256(b *testing.B) {
+	benchmarkRender(b)
+}
+
+func BenchmarkRender512(b *testing.B) {
+	benchmarkRender(b)
+}
+
+func BenchmarkRender1024(b *testing.B) {
+	benchmarkRender(b)
+}
+
+func BenchmarkRender2048(b *testing.B) {
 	benchmarkRender(b)
 }
