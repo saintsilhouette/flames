3e3194fa9b3ea50179e41438d22012870e5b0c9c
setanoier <setanoier@gmail.com>
2024-12-24 18:25:38 +0300

    refactor: delete redundant input method

--- internal/infrastructure/adapter/adapter.go
+++ internal/infrastructure/adapter/adapter.go
@@ -1,37 +1,23 @@
 package adapter
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"log/slog"
-	"strings"
 )
 
 // IOAdapter is the additional layer for the i/o operations.
 type IOAdapter struct {
-	w       io.Writer
-	r       io.Reader
-	scanner *bufio.Scanner
-	logger  *slog.Logger
+	w      io.Writer
+	logger *slog.Logger
 }
 
 // New instantiates a new IOAdapter entity.
-func New(w io.Writer, r io.Reader, logger *slog.Logger) *IOAdapter {
+func New(w io.Writer, logger *slog.Logger) *IOAdapter {
 	return &IOAdapter{
-		w:       w,
-		r:       r,
-		scanner: bufio.NewScanner(r),
-		logger:  logger,
+		w:      w,
+		logger: logger,
 	}
-}
-
-// Input reads data from the provided io.Reader.
-func (a *IOAdapter) Input() string {
-	a.scanner.Scan()
-	input := a.scanner.Text()
-
-	return strings.Trim(input, "\n")
 }
 
 // Output writes data to the provided io.Writer.
